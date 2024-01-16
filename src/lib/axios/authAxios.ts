ca7388b29ca86ed1c75ef3d40e0f0962650c7fbb
Damar Anshary <damar.galihanshary7@gmail.com>
2024-01-16 21:35:03 +0700

    add login and register form

--- src/lib/axios/authAxios.ts
+++ src/lib/axios/authAxios.ts
@@ -5,26 +5,32 @@ import {
   RegisterResponse,
 } from "../../interfaces/userInterfaces";
 
-export const Login = async ({
+export const login = async ({
   email,
   password,
 }: LoginSchema): Promise<LoginResponse> => {
-  const response = await axios.post(`${import.meta.env.API_URL}/login`, {
-    email,
-    password,
-  });
+  const response = await axios.post(
+    `${import.meta.env.VITE_API_URL}/auth/login`,
+    {
+      email,
+      password,
+    }
+  );
 
   return response.data;
 };
 
-export const Register = async ({
+export const register = async ({
   email,
   password,
 }: LoginSchema): Promise<RegisterResponse> => {
-  const response = await axios.post(`${import.meta.env.API_URL}/register`, {
-    email,
-    password,
-  });
+  const response = await axios.post(
+    `${import.meta.env.VITE_API_URL}/auth/register`,
+    {
+      email,
+      password,
+    }
+  );
 
   return response.data;
 };
--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "../pages/admin/Dashboard";
+import Register from "../pages/user/Register";
+import Login from "../pages/user/Login";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/">
         <Route index element={<div>Home</div>} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
         <Route path="admin">
           <Route path="dashboard" element={<Dashboard />} />
         </Route>
